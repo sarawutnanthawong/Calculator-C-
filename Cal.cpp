95dc0af75421dd50bbf02999ea80cbda4c6d1f19
Naphon intusai <npitz.ze@gmail.com>
2026-02-04 15:02:11 +0700

    Prototype Main

---
+++ Cal.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// --- Prototypes (ประกาศฟังก์ชันไว้ก่อน) ---
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+float divide(int a, int b);
+
+int main() {
+    int num1, num2;
+    float result;
+    char op;
+
+    return 0;
+}
