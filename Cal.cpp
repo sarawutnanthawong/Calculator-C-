032f0efb40e512758e38cce8d97e74e4cd5910a9
jaylyn <apisitjf2549@gmail.com>
2026-02-04 14:53:07 +0700

    add plus fuction

--- Cal.cpp
+++ Cal.cpp
@@ -10,6 +10,30 @@ int main() {
     float result;
     char op;
 
+    printf("simple Calculator (+ , - , * , /)\n");
+
+    printf("enter first num: ");
+    scanf("%d", &num1);
+
+    printf("enter operator (+ , - , * , /): ");
+    scanf(" %c", &op);
+
+    printf("enter second num: ");
+    scanf("%d", &num2);
+
+    printf("\nprocess:\n");
+
+    if (op == '+') {
+        result = add(num1, num2);
+        printf("%d + %d\n", num1, num2);
+        printf("result:\n%.0f\n", result);
+
     return 0;
+}
+
+}
+
+int add(int a, int b) {
+    return a + b;
 }
 
