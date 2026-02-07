766213d6d62890541c0c36a767651028eb0c5430
Naphon intusai <npitz.ze@gmail.com>
2026-02-07 21:03:06 +0700

    fix multiply

--- Cal.cpp
+++ Cal.cpp
@@ -5,49 +5,53 @@ int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
 
-int main() {
+int main()
+{
     int num1, num2;
-    float result; 
+    float result;
     char op;
 
-    printf("Simple Calculator (+ , - , *)\n");
-
+    printf("simple Calculator (+ , - , * , /)\n");
     printf("enter first num: ");
     scanf("%d", &num1);
-
-    printf("enter operator (+ , - , *): ");
+    printf("enter operator (+ , - , * , /): ");
     scanf(" %c", &op);
-
     printf("enter second num: ");
     scanf("%d", &num2);
-
     printf("\nprocess:\n");
-
-    if (op == '+') {
+    printf("\nprocess:\n");
+    
+    if (op == '+')
+    {
         result = add(num1, num2);
         printf("%d + %d\n", num1, num2);
         printf("result:\n%.0f\n", result);
-    } 
-    else if (op == '-') {
+    }
+    else if (op == '-')
+    {
         result = sub(num1, num2);
         printf("%d - %d\n", num1, num2);
         printf("result:\n%.0f\n", result);
-    } 
-    else if (op == '*') {
-        result = mul(num1, num2); 
-        printf("%d * %d\n", num1, num2); 
-        printf("result:\n%.0f\n", result); 
-    } 
+    }
+    else if (op == '*')
+    {
+        result = mul(num1, num2);
+        printf("%d * %d\n", num1, num2);
+        printf("result:\n%.0f\n", result);
+    }
     return 0;
-} 
-int add(int a, int b) {
+}
+int add(int a, int b)
+{
     return a + b;
 }
 
-int sub(int a, int b) {
+int sub(int a, int b)
+{
     return a - b;
 }
 
-int mul(int a, int b) { 
-    return a * b; 
+int mul(int a, int b)
+{
+    return a * b;
 }
